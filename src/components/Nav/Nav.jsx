da10bc4b1f2535eefc6e35bfeda1362ee7e787c8
JESUS EUGENIO RICARDEZ <jericrdez.c03@ucamp.utel.edu.mx>
2022-01-29 08:33:16 -0600

    actualización
    Se modifico el componente Navbar, para que fuera responsivo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,46 +1,54 @@
 import React from 'react'
 import './Nav.css'
 import { Link } from 'react-router-dom'
-import logo from './img/logo.png';
+import logo from './img/logo.png'
+import menuS from './img/Menu_icon.png'
 
 export const Nav = () => {
+
+  
   return (
     <div>
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            <img src={logo} alt="Logo" style={{ width: '80px' }}></img>
+      <nav className="nav-style">
+        <div className="container-logo">
+          <Link to="/">
+            <img className='logo' src={logo} alt="Logo" style={{ width: '75px' }}></img>
           </Link>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link  navbar-font" to="/nosotros">
+        </div>
+       
+          
+        <ul id="list" className="navbar-list">
+          <li className="navbar-item">
+                <Link className="navbar-font" to="/nosotros">
                   Nosotros
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link  navbar-font" to="/contacto">
+          </li>
+
+          <li className="navbar-item">
+                <Link className="navbar-font" to="/contacto">
                   Contacto
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link  navbar-font" to="/menu">
+          </li>
+
+          <li className="navbar-item">
+                <Link className="navbar-font" to="/menu">
                   Menu
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link  navbar-font" to="/reservar">
+          </li>
+
+          <li className="navbar-item">
+                <Link className="navbar-font" to="/reservar">
                   Reservar
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link  navbar-font" to="/gestion">
+          </li>
+
+          <li className="navbar-item">
+                <Link className="navbar-font" to="/gestion">
                   Gestion
                 </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+          </li>
+
+        </ul>
       </nav>
     </div>
   )
